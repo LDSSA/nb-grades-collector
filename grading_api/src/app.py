125a801d99ff5196ceddc76091608a494b282614
InesPessoa <inesmspessoa@gmail.com>
2021-03-23 22:51:44 +0000

    update endpoint was removed

--- grading_api/src/app.py
+++ grading_api/src/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from routes import register_grade, create_users, update_info
+from routes import register_grade, create_users
 
 
 app = Flask(__name__)
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 app.register_blueprint(register_grade)
 app.register_blueprint(create_users)
-app.register_blueprint(update_info)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
