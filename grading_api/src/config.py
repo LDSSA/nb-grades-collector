dee8a5796340b30b6e8025205c7830e26e892257
InesPessoa <inesmspessoa@gmail.com>
2021-03-26 00:35:11 +0000

    configs to work on server

--- grading_api/src/config.py
+++ grading_api/src/config.py
@@ -2,7 +2,7 @@ import os
 from playhouse.db_url import connect
 
 #local
-DB = connect('sqlite:///resources/predictions.db')
+#DB = connect('sqlite:///resources/predictions.db')
 
 #server
-#DB = connect(os.environ.get('DATABASE_URL'))
+DB = connect(os.environ.get('DATABASE_URL'))
